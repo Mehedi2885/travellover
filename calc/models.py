9e83ec973fef1a5f4f8aed9d8a2cacfcab744bc9
Mehedi2885 <47429333+Mehedi2885@users.noreply.github.com>
2021-07-08 18:47:40 -0400

    Initial commit

---
+++ calc/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+# For travelo information
+
+#id: int
+#name: str
+#price: int
+#image: str
+#des: str
+#offer: bool
+
+
---
+++ calc/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+import re
+
+
+# Create your views here.
+
+def home(request):
+    return render(request, 'home.html', {'name': 'Mehedi'})
+
+
+def add(request):
+    val1 = eval(request.POST["num1"])
+    val2 = eval(request.POST["num2"])
+
+    res = val1 + val2
+
+    return render(request, 'result.html', {'result': res})
